3190a1295a2e3775742176afd7cee96defd4a2d2
Mark van Beusekom <mark@home>
2025-11-11 16:32:41 +0100

    added goto definition lsp keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -52,3 +52,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
