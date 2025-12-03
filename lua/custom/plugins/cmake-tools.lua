b1856c582776d9fab9bcb672c293fd4dccc59fb4
Mark van Beusekom <mark@home>
2025-12-03 13:15:00 +0100

    standard build dir set to default

--- lua/custom/plugins/cmake-tools.lua
+++ lua/custom/plugins/cmake-tools.lua
@@ -1,8 +1,19 @@
 return {
   'Civitasv/cmake-tools.nvim',
-  opts = {
-    cmake_compile_commands_options = {
-      target = vim.loop.cwd() .. '/build',
-    },
-  },
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    print 'cmake-tools config loaded!'
+    require('cmake-tools').setup {
+      -- Force build directory
+      cmake_build_directory = 'build',
+
+      -- Generate compile_commands and symlink to project root
+      cmake_generate_options = { '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON' },
+      cmake_compile_commands_options = {
+        action = 'soft_link', -- or "copy", "lsp", "none"
+        target = vim.loop.cwd(), -- typically project root
+      },
+    }
+  end,
 }
