efc2142d965b05d226ed40350d066467e85530b1
Mark van Beusekom <mark@home>
2026-06-24 17:05:40 +0200

    fixed cmake-tools and toggleterm

--- lua/custom/plugins/cmake-tools.lua
+++ lua/custom/plugins/cmake-tools.lua
@@ -1,19 +1,33 @@
-return {
-  'Civitasv/cmake-tools.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+-- CMake Tools for Neovim
+-- https://github.com/Civitasv/cmake-tools.nvim
 
-  config = function()
-    print 'cmake-tools config loaded!'
-    require('cmake-tools').setup {
-      -- Force build directory
+vim.pack.add {
+  { src = 'https://github.com/Civitasv/cmake-tools.nvim' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+}
+
+-- Configure AFTER plugins are loaded
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'VeryLazy',
+  once = true,
+  callback = function()
+    local ok, cmake = pcall(require, 'cmake-tools')
+    if not ok then
+      vim.notify('cmake-tools failed to load', vim.log.levels.ERROR)
+      return
+    end
+
+    cmake.setup {
       cmake_build_directory = 'build',
 
-      -- Generate compile_commands and symlink to project root
-      cmake_generate_options = { '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON' },
+      cmake_generate_options = {
+        '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
+      },
+
       cmake_compile_commands_options = {
-        action = 'soft_link', -- or "copy", "lsp", "none"
-        target = vim.loop.cwd(), -- typically project root
+        action = 'soft_link',
+        target = vim.loop.cwd(),
       },
     }
   end,
-}
+})
--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,11 +1,12 @@
-return {
-  {
-    'akinsho/toggleterm.nvim',
-    opts = {
-      size = 20,
-      open_mapping = [[<leader>\]],
-      direction = 'horizontal',
-    },
-    version = '*',
-  },
+-- ToggleTerm: terminal management for Neovim
+-- https://github.com/akinsho/toggleterm.nvim
+
+vim.pack.add {
+  'https://github.com/akinsho/toggleterm.nvim',
+}
+
+require('toggleterm').setup {
+  size = 20,
+  open_mapping = [[<leader>\]],
+  direction = 'horizontal',
 }
